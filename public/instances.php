e784d9069d4d89e181cb32aa794dbfa5d5141336
Tomasz Klim <github@tomaszklim.pl>
2020-01-15 23:49:26 +0100

    show local ip of ec2/ecs instances without public ip address

--- public/instances.php
+++ public/instances.php
@@ -38,6 +38,35 @@ function get_instance_link($vendor, $account, $region, $id) {
 	return "<a href=\"aws-instance.php?account=$enc1&region=$enc2&id=$enc3\">$id</a>";
 }
 
+function get_aws_instance_local_ip($account, $region, $id) {
+	global $_data_path;
+	$file = "$_data_path/inventory/raw-aws-instances-$account-$region.json";
+	if (!file_exists($file) || filesize($file) < 30)
+		return false;
+
+	$json = file_get_contents($file);
+	$data = json_decode($json, true);
+
+	foreach ($data["Reservations"] as $reservation)
+		foreach ($reservation["Instances"] as $instance)
+			if ($instance["InstanceId"] == $id)
+				return $instance["PrivateIpAddress"];
+
+	return false;
+}
+
+function get_hostname($vendor, $account, $region, $public_hostname, $id) {
+	if ($public_hostname != "-" || $vendor != "aws")
+		return $public_hostname;
+
+	$region = substr($region, 0, -1);
+	$ip = get_aws_instance_local_ip($account, $region, $id);
+	if ($ip)
+		return "local/$ip";
+	else
+		return $public_hostname;
+}
+
 
 $file = "$_data_path/inventory/instances.list";
 $date = date("Y-m-d H:i:s", filemtime($file));
@@ -78,22 +107,24 @@ foreach ($lines as $line) {
 	$vendor = $tmp[0];
 	$account = $tmp[1];
 	$state = $tmp[3];
+	$region = $tmp[5];
+	$id = $tmp[7];
 	$style = ($state != "running" ? "background-color: #f4cccc;" : false);
 
 	table_row(array(
 		$vendor,
 		get_account_link($vendor, $account),
-		$tmp[2],  // hostname/ip
+		get_hostname($vendor, $account, $region, $tmp[2], $id),  // [2] - raw hostname
 		$state,
 		$tmp[9],  // created date
 		str_replace(";", "<br />", $tmp[10]),  // tag(s)/label(s) - printed line by line
 		$tmp[4],  // ssh key
-		get_region_link($vendor, $account, $tmp[5]),
+		get_region_link($vendor, $account, $region),
 		$tmp[6],  // instance type
-		get_instance_link($vendor, $account, $tmp[5], $tmp[7]),  // instance id
+		get_instance_link($vendor, $account, $region, $id),
 		get_image_name($vendor, $tmp[8]),
 		@$tmp[11],  // optional vpc-id/project
-		map_acl_to_ranges($vendor, $account, $tmp[5], 22, @$tmp[12]),  // optional list of security groups (not divided by spaces)
+		map_acl_to_ranges($vendor, $account, $region, 22, @$tmp[12]),  // optional list of security groups (not divided by spaces)
 	), $style);
 }
 
